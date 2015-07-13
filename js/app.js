25fbc117fd0bd02fb349262239db64d2c17fe053
Sam Richard <snugug@gmail.com>
2015-07-13 10:49:52 -0400

    Add JavaScript

---
+++ js/app.js
@@ -0,0 +1,17 @@
+(function (Prism) {
+  'use strict';
+
+  var fired = false;
+
+  window.addEventListener('DOMContentLoaded', function () {
+    if (!fired) {
+      Prism.highlightAll();
+      fired = true;
+    }
+  });
+
+  if (!fired) {
+    Prism.highlightAll();
+    fired = true;
+  }
+}(window.Prism));
