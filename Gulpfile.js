9c81222bf2662d0f01c4ec351ce73477bf5713bf
Sam Richard <snugug@gmail.com>
2015-12-25 10:53:11 -0500

    Upgrade to latest Armadillo

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,22 +5,4 @@
 //////////////////////////////
 var gulp = require('gulp');
 
-require('gulp-armadillo')(gulp, {
-  'pages': {
-    'dir': 'pages/musings',
-    'transformURL': true,
-    'sort': 'published',
-    'reverse': true
-  },
-  'copy': {
-    'dist': [
-      '.www/**/*',
-      '!.www/**/*.html',
-      '!.www/bower_components/**/*',
-      '!.www/css/**/style.css',
-      '!.www/js/**/*',
-      '.www/css/fonts.css',
-      'CNAME'
-    ]
-  }
-});
+require('gulp-armadillo')(gulp);
