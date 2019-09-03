72444ed16288578c7bc55ebb924d0e76678f55ec
Sam Richard <snugug@google.com>
2019-09-03 11:00:37 -0500

    :crystal_ball: Log error, add user

--- deploy.js
+++ deploy.js
@@ -8,12 +8,17 @@ ghpages.publish(
     repo: `https://${token}@github.com/Snugug/blog.git`,
     // silent: true,
     message: ':shipit: Deploy to Pages',
+    user: {
+      name: 'Deploy Bot',
+      email: 'sam@snug.ug',
+    },
   },
   err => {
     if (err) {
       console.error('FAILURE');
-      const tokenRegex = new RegExp(token, 'gm');
-      console.error(err.replace(tokenRegex, 'GH_TOKEN'));
+      console.error(err);
+      // const tokenRegex = new RegExp(token, 'gm');
+      // console.error(err.replace(tokenRegex, 'GH_TOKEN'));
       process.exit(2);
     }
     console.log('Deployed');
