3c8ac6aafc398ac226dbd0d2a122356319a5a3a9
Sam Richard <snugug@google.com>
2019-09-03 08:28:30 -0500

    :new: Add Netifly CMS

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,5 +4,17 @@
 // Require Gulp and grab Armadillo
 //////////////////////////////
 const gulp = require('gulp');
+const rename = require('gulp-rename');
 
 require('gulp-armadillo')(gulp);
+
+gulp.task('config:cms', () => {
+  return gulp
+    .src('cms.yml')
+    .pipe(
+      rename({
+        basename: 'config',
+      }),
+    )
+    .pipe(gulp.dest('.www/admin'));
+});
