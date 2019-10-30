0d727e454f723461cf63cdb91b0a26b1cf3ca87c
Sam Richard <snugug@google.com>
2019-10-30 08:35:06 -0400

    :new::green_heart: Add redirect generation at end

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,8 @@
 //////////////////////////////
 const gulp = require('gulp');
 const rename = require('gulp-rename');
+const fs = require('fs-extra');
+const path = require('path');
 
 require('gulp-armadillo')(gulp);
 
@@ -13,8 +15,24 @@ gulp.task('config:cms', () => {
     .src('cms.yml')
     .pipe(
       rename({
-        basename: 'config',
-      }),
+        basename: 'config'
+      })
     )
     .pipe(gulp.dest('.www/admin'));
+});
+
+const redirects = {
+  cooking: '/cookbook'
+};
+
+async function makeRedirect(input, output) {
+  const file = `<!DOCTYPE html><meta charset="utf-8" /><title>Redirecting to ${output}</title><meta http-equiv="refresh" content="0; URL=${output}" /><link rel="canonical" href="${output}" />`;
+  const p = path.join('.www', input, 'index.html');
+  return fs.outputFile(p, file);
+}
+
+gulp.task('generate:redirects', cb => {
+  const rdrs = Object.keys(redirects).map(input => makeRedirect(input, redirects[input]));
+
+  Promise.all(rdrs).then(() => cb());
 });
