8359086eb202e10000d5b077de3348dee283afa7
Sam Richard <snugug@gmail.com>
2022-11-25 16:14:45 -0500

    🆕(cms) Add CMS to codebase (#65)
    * Initial CMS commit
    
    * CMS CI attempt
    
    * Add badge to README
    
    * Add CMS to PR Title Checker

---
+++ cms/schemas/index.js
@@ -0,0 +1,6 @@
+import page from './page';
+import post from './post';
+import recipe from './recipe';
+import category from './category';
+
+export const schemaTypes = [post, recipe, page, category];
