77ba8dcb8fa6f6fc4b10386962bcad7dcb0899b9
Sam Richard <snugug@google.com>
2025-12-18 07:25:50 -0500

    Flatten folder structure

--- cms/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import page from './page';
-import post from './post';
-import recipe from './recipe';
-import category from './category';
-
-export const schemaTypes = [post, recipe, page, category];
